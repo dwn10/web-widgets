1e02a0d4abc352aa0b6ed380f5af574217d1028e
Darwin Paz <142513909+dwn10@users.noreply.github.com>
2023-12-27 11:28:18 +0100

    suscripcion-ok

--- WEBSITE/TEXTO-A-VOZ/script.js
+++ WEBSITE/TEXTO-A-VOZ/script.js
@@ -1,6 +1,8 @@
 
+// crea un simple prototipo para sintetizar texto en voz alta utilizando el API SpeechSynthesis del navegador
 let speech = new SpeechSynthesisUtterance();
 
+// Obtención y almacenamiento de voces disponibles
 let voices = [];
 
 let voiceSelect = document.querySelector("select");
@@ -12,10 +14,12 @@ window.speechSynthesis.onvoiceschanged = () => {
     voices.forEach((voice, i) => (voiceSelect.options[i] = new Option(voice.name, i)));
 };
 
+// Manejo del cambio de voz
 voiceSelect.addEventListener("change", () => {
     speech.voice = voices[voiceSelect.value];
 });
 
+// Activación de la síntesis de voz
 document.querySelector("button").addEventListener("click", () => {
     speech.text = document.querySelector("textarea").value;
     window.speechSynthesis.speak(speech);
